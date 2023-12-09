4ac48f53fa07f60a3d6350232be1580f3246b516
MunawarRizwan <syed.munawar@mindler.com>
2023-12-10 00:24:44 +0530

    Feat : UI

--- src/pages/connect-account/ConnectAccount.tsx
+++ src/pages/connect-account/ConnectAccount.tsx
@@ -1,6 +1,10 @@
+
 import React, { useState } from 'react'
 import { ethers, } from 'ethers';
 import "./ConnectAccount.scss";
+import LeftBanner from '../onboarding/left-banner/LeftBanner';
+import logo from "./../../assets/images/logo.jpeg"
+
 declare global {
     interface Window { ethereum: any; }
 }
@@ -26,7 +30,7 @@ const ConnectAccount = () => {
 
                 const provider = new ethers.BrowserProvider(window.ethereum);
                 let balance = await provider.getBalance(accounts[0]);
-                let fmtBlc =  ethers.formatUnits(balance);
+                let fmtBlc = ethers.formatUnits(balance);
                 setWalletBalance(fmtBlc);
 
             } catch (error) {
@@ -42,14 +46,25 @@ const ConnectAccount = () => {
 
 
     return (
-        <div className='connect-acc-page'>
-            <div className='wrapper tw-flex tw-justify-center tw-flex-col tw-items-center tw-gap-10'>
-                <p className='tw-text-2xl tw-font-bold tw-text-secondary tw-italic tw-text-center'>Connect with your Account to get started with the ....</p>
-                <div className='tw-flex tw-justify-center tw-flex-col tw-items-center'>
-                    <button onClick={reqEthAccount} className='tw-flex tw-items-center tw-text-lg btn btn--blue'> <img className='tw-w-10 tw-mr-2' src='/metamask.png'></img> Connect Your Account</button>
-                    <p className='tw-text-xl tw-font-semibold tw-my-4' >Your wallet address is - {WA}</p>
+        <div className='connect-acc-page tw-flex '>
 
-                    <p className='tw-text-lg'>Current Balance: {walletBalance}</p>
+            <LeftBanner />
+            <div>
+                <div className='tw-flex tw-gap-6 tw-justify-end tw-items-center'>
+                    <p className='section-heading survey-heading'>SurveyX</p>
+                    <div className='survey-logo-container'>
+                        <img className='logo' src={`${logo}`}></img>
+                    </div>
+                </div>
+
+                <div className='wrapper tw-flex tw-justify-center tw-flex-col tw-items-center tw-gap-10 tw-p-4'>
+                    <p className='tw-text-2xl tw-font-bold tw-text-secondary tw-italic tw-text-center'>Connect with your Account to get started with the ....</p>
+                    <div className='tw-flex tw-justify-center tw-flex-col tw-items-center'>
+                        <button onClick={reqEthAccount} className='tw-flex tw-items-center tw-text-lg btn btn--blue'> <img className='tw-w-10 tw-mr-2' src='/metamask.png'></img> Connect Your Account</button>
+                        <p className='tw-text-xl tw-font-semibold tw-my-4' >Your wallet address is - {WA}</p>
+
+                        <p className='tw-text-lg'>Current Balance: {walletBalance}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/onboarding/left-banner/LeftBanner.tsx
+++ src/pages/onboarding/left-banner/LeftBanner.tsx
@@ -1,45 +1,41 @@
 import React from 'react'
 import Carousel from 'better-react-carousel'
 import "./LeftBanner.scss"
-const imageBaseUrl = "https://mindler-products-images.imgix.net/confluence";
+import Image1 from "./../../../assets/images/landing_1.jpeg"
+import Image2 from "./../../../assets/images/landing_2.jpeg"
+import Image3 from "./../../../assets/images/landing_3.jpeg"
+import Image4 from "./../../../assets/images/landing_4.jpeg"
 
+
+
+
+
+const imageBaseUrl = "https://mindler-products-images.imgix.net/confluence";
 
 const LeftBanner = () => {
     return (
-        <div className='left-section tw-ml-4 tw-mt-4 md:tw-w-1/2 tw-hidden md:tw-flex tw-flex-col tw-items-center md:tw-p-16'>
+        <div className='left-section tw-ml-4 tw-mt-4 md:tw-w-1/2 tw-hidden md:tw-flex tw-flex-col tw-items-center md:tw-p-4'>
             <Carousel cols={1} rows={1} loop={true} showDots={true} dotColorActive="#1480B7" autoplay={5000}>
                 <Carousel.Item>
-                    <div className=''>
-                        <img src={`${imageBaseUrl}/onboarding/youngBoy.png`} alt='' className='' />
-                    </div>
-                    <div className='section-heading tw-mt-4 tw-text-center'>
-                        Enable students to identify their best-fit career with our world-class career assessment and personalised guidance.
+                    <div className='logo-container'>
+                        <img src={`${Image1}`} alt='' className='logo' />
                     </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <div>
-                        <img src={`${imageBaseUrl}/onboarding/girlOne.png`} alt='' className='' />
-                    </div>
-                    <div className='section-heading tw-mt-4 tw-text-center'>
-                        Empower students to learn all about the professional world with virtual career simulations, exhaustive career library, career blogs and vlogs.
+                    <div className='logo-container'>
+                        <img src={`${Image2}`} alt='' className='logo' />
                     </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <div>
-                        <img src={`${imageBaseUrl}/onboarding/girlTwo.png`} alt='' className='' />
-                    </div>
-                    <div className='section-heading tw-mt-4 tw-text-center'>
-                        Pave student’s way to their dream college with our end-to-end college application guidance, scholarship drive and corporate internship program.
+                    <div className='logo-container'>
+                        <img src={`${Image3}`} alt='' className='logo' />
                     </div>
                 </Carousel.Item>
-                {/* <Carousel.Item>
-                    <div>
-                        <img src={`${imageBaseUrl}/onboarding/youngKids.png`} alt='' className='' />
+                <Carousel.Item>
+                    <div className='logo-container'>
+                        <img src={`${Image4}`} alt='' className='logo' />
                     </div>
-                    <div className='section-heading tw-mt-4 tw-text-center'>
-                        Enable schools in creating a career guidance ecosystem in sync with the vision of New Education Policy
-                    </div>
-                </Carousel.Item> */}
+                </Carousel.Item>
             </Carousel>
 
         </div>
